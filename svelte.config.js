9d03fb4d87a15a60371be382cd40830f5288e392
Clayton Ferris <cferris@gmail.com>
2023-10-27 08:20:16 -0700

    setup fastly compute adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'sveltekit-adapter-fastly';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
